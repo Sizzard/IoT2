623d9be56adf8c3c0e6d8f16dc2a469041194635
Sizzard <sizzards01@gmail.com>
2026-07-15 05:57:05 -0400

    Changed script p3 to install docker

--- p3/setup.sh
+++ p3/setup.sh
@@ -2,18 +2,25 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
+##### Install Docker
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc   
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update   
 
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl start docker
+sudo systemctl enable docker
+#####
 
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
@@ -33,6 +40,8 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 
 kubectl apply -n argocd -f app.yml
 
-# Launching server and expose port for accessing via website              kubectl port-forward svc/argocd-server -n argocd 8080:443
+# Launching server and expose port for accessing via website:             kubectl port-forward svc/argocd-server -n argocd 8080:443
 
-# PASSWORD COMMAND :            kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > password.txt && echo "$(cat password.txt)"
+# Password command:                                                       kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > password.txt && echo "$(cat password.txt)"
+
+# Connection command:                                                     curl http://localhost:30088
