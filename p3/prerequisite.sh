b73b970c270077aaae61245e159f02e1b9c54005
Sizzard <sizzards01@gmail.com>
2026-07-15 06:19:34 -0400

    Changed scripts in p3

--- p3/prerequisite.sh
+++ p3/prerequisite.sh
@@ -24,12 +24,3 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 newgrp docker
 #####
-
-##### Install K8s and K3d
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-#####
--- p3/setup_app.sh
+++ p3/setup_app.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##### Install K8s and K3d
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+#####
+
 k3d cluster create argocd --port "30088:30088@server:0"
 
 kubectl create namespace argocd
